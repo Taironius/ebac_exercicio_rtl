dac1f1977795b942915e68c35e29281027156fa4
Kenedy Jardim <kenedydacosta@gmail.com>
2024-01-26 12:57:16 -0300

    teste de inserção de 02 comentarios

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+      <Post imageUrl="https://http2.mlstatic.com/D_NQ_NP_2X_875587-MLB73480776372_122023-F.webp">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
     </div>
--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,23 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+    test('Inserindo 02 Comentarios', () => {
+        const { debug } = render(<PostComment />);
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Indestrutivel'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        fireEvent.change(screen.getByTestId('campo-comentario'), {
+            target: {
+                value: 'Resistente'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+        debug()
+        expect(screen.getByText('Indestrutivel')).toBeInTheDocument()
+        expect(screen.getByText('Resistente')).toBeInTheDocument()
+    });
 });
